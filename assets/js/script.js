1a04d974672af03a530578453cd996c48175dd3d
abinow <abinow.mail@gmail.com>
2024-03-27 14:57:11 +0530

    leads website started ~ added preloader ~ next add an effect in nav-link

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/////////preloader function
+var load = document.getElementById("preloader");
+let header = document.getElementById("hdr");
+function isHeaderLoaded() {
+  return header !== null; 
+}
+// Check if header is loaded, and if so, add classes immediately
+if (isHeaderLoaded()) {
+  setTimeout(function () {
+      load.classList.add('hide');
+  }, 500);
+}
+
+/////////preloader show on every a tag clicked
+const links = document.querySelectorAll('a.link');
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('preloader').classList.add('show');
+        document.getElementById('preloader').classList.remove('hide');
+        // Redirect to the href attribute after showing the preloader
+        setTimeout(() => { // Adding a delay to simulate preloader
+            window.location.href = link.getAttribute('href');
+        }, 1000); // Adjust the delay as needed
+    });
+});
